422fa393fece38764c0a8e68ae346a95ab846cfc
Spivakov <alexander.spivakov@c3dlabs.com>
2023-12-23 22:43:26 +0300

    Exam ticket 1

---
+++ source/library/speaker.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#ifdef WIN32
+  #include <windows.h>
+  extern "C" __declspec(dllexport) void LogNameAge( const char* name, const char* age );
+#else
+  extern "C" void LogNameAge( const char* name, const char* age );
+#endif
