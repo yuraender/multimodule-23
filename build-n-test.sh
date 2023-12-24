8628e0d7a1d922414e3ee00d0af19a544f7e731e
Spivakov <alexander.spivakov@c3dlabs.com>
2023-12-24 12:15:25 +0300

    exam ticket 3

---
+++ build-n-test.sh
@@ -0,0 +1,6 @@
+﻿#build the project and run the app
+mkdir build
+cmake -S Source -B build
+cmake --build build --config=Debug
+cd build/bin
+./demoapplication JohnDow 44
---
+++ source/library/speaker.h
@@ -0,0 +1,6 @@
+﻿#ifdef WIN32
+  #include <windows.h>
+  extern "C" __declspec(dllexport) void LogNameAge( const char* name, const char* age );
+#else
+  extern "C" void LogNameAge( const char* name, const char* age );
+#endif
