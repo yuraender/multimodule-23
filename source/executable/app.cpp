55c19dd59ba758593fdae0d9a92489c8badf413d
Spivakov <alexander.spivakov@c3dlabs.com>
2023-12-24 12:04:54 +0300

    exam ticket 2

---
+++ source/executable/app.cpp
@@ -0,0 +1,15 @@
+﻿#include <speaker.h>
+
+int main( int argc, const char ** argv )
+{
+  if ( argc == 3 )
+  {
+  	LogFormattedNameAge( argv[1], argv[2] );
+  	return 0;
+  }
+  else
+  {
+  	LogFormattedNameAge( 0, 0 );
+  	return argc;
+  }
+}
---
+++ source/library/speaker.cpp
@@ -0,0 +1,19 @@
+﻿#include "speaker.h"
+#include <stdio.h>
+
+static void LogFormattedNameAge( const char* name, const char* age )
+{
+  printf("Hi, my name is %s I'm %s year old. What about you?\n", name, age );
+}
+
+void LogNameAge( const char* name, const char* age )
+{
+  if( name && age )
+  {
+  	LogFormattedNameAge( name, age );
+  }
+  else
+  {
+  	printf("*** INVALID PARAMETERS ***\n" );
+  }
+}
