c3c9365bef42e4f3c6d52c3623920f809206fca1
YuraEnder <yuraender@yandex.ru>
2024-01-30 16:06:18 +0300

    Fix errors

--- build-n-test.sh
+++ build-n-test.sh
@@ -1,6 +1,6 @@
 ﻿#build the project and run the app
 mkdir build
-cmake -S build -B Source
-cmake --build build --config=Debug
+cmake -S source -B build
+cmake --build build --config Debug
 cd build/bin
 ./demoapplication JohnDow 44
